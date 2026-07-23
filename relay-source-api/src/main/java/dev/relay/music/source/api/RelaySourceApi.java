134fd135e0f8e6b764e103b6ba3b7789e568f719
Livio <livio.macaj@cit.edu.al>
2026-07-23 10:14:16 +0200

    Declare source extension manifest identity

--- relay-source-api/src/main/java/dev/relay/music/source/api/RelaySourceApi.java
+++ relay-source-api/src/main/java/dev/relay/music/source/api/RelaySourceApi.java
@@ -2,7 +2,10 @@ package dev.relay.music.source.api;
 
 public final class RelaySourceApi {
     public static final int VERSION = 1;
+    public static final String EXTENSION_FEATURE = "dev.relay.music.source.extension";
+    public static final String METADATA_API_VERSION = "relay.source.api";
+    public static final String METADATA_ENTRY_CLASS = "relay.source.class";
+    public static final String METADATA_EXTENSION_ID = "relay.source.id";
 
     private RelaySourceApi() {}
 }
-
